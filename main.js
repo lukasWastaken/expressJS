61fbdb2d26a6409c2b3111dfa407173866dc2ea2
lukas <lukaswastaken@icloud.com>
2024-07-06 22:53:05 +0200

    Cleand main.js up

--- main.js
+++ main.js
@@ -2,7 +2,6 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-// Setzen der View-Engine auf EJS
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
 app.set('views', path.join(__dirname, 'views'));
@@ -10,9 +9,11 @@ app.set('views', path.join(__dirname, 'views'));
 app.get('/', (req, res) => {
   res.render('index.html');
 });
+
 app.get('/test', (req, res) => {
     res.render('test.html')
 });
+
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
 });
