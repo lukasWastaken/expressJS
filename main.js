7d2fd1a37b043c7012d5fd26c05941fc3ee6994f
lukas <lukaswastaken@icloud.com>
2024-07-07 11:32:14 +0200

    Changed by Lukas: - Implemented Login - Implemented Register - Implemented MongoDB Connection (localhost) - Created new html files

--- main.js
+++ main.js
@@ -1,17 +1,52 @@
 const express = require('express');
 const path = require('path');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const User = require('./models/User');
 const app = express();
+
+mongoose.connect('mongodb://localhost:27017/squaresphere', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
 
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
 app.set('views', path.join(__dirname, 'views'));
+
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
   res.render('index.html');
 });
 
 app.get('/test', (req, res) => {
-    res.render('test.html')
+  res.render('test.html');
+});
+
+app.get('/register', (req, res) => {
+  res.render('register.html');
+});
+
+app.post('/register', async (req, res) => {
+  const { username, password } = req.body;
+  const user = new User({ username, password });
+  await user.save();
+  res.redirect('/login');
+});
+
+app.get('/login', (req, res) => {
+  res.render('login.html');
+});
+
+app.post('/login', async (req, res) => {
+  const { username, password } = req.body;
+  const user = await User.findOne({ username });
+  if (user && await user.comparePassword(password)) {
+    res.send('Login erfolgreich!');
+  } else {
+    res.send('Login fehlgeschlagen.');
+  }
 });
 
 app.listen(3000, () => {
