72e9535143642f59144283273792240a3f0c859a
lukas <sl3mey@gmail.com>
2024-07-31 23:23:16 +0200

    Disabled rightclick on every page

--- main.js
+++ main.js
@@ -342,5 +342,5 @@ app.delete('/api/releases/:id', async (req, res) => {
 });
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on  http://localhost:${PORT}`);
 });
