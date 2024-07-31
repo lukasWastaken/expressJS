ad74fbb0542ac1a308e48f8a3bcca570255fb110
lukas <lukaswastaken@icloud.com>
2024-07-31 19:35:00 +0200

    Fixed two bugs

--- main.js
+++ main.js
@@ -282,5 +282,5 @@ app.get('/files/:filename', (req, res) => {
 });
 
 app.listen(3000, () => {
-  console.log('Server started on port 3000');
+  console.log('Server started on  http://localhost:3000');
 });
